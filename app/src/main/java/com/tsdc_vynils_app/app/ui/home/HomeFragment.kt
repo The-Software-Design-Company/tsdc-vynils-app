2a707f48415fc4be9e7fc027aa5df4665fa3d247
migueltovar <loganitsolutions@outlook.com>
2023-11-04 15:27:57 -0500

    ajustando iconos e imagen de no conexión

--- app/src/main/java/com/tsdc_vynils_app/app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/tsdc_vynils_app/app/ui/home/HomeFragment.kt
@@ -7,9 +7,11 @@ import android.graphics.Paint
 import android.graphics.RectF
 import android.graphics.Shader
 import android.graphics.drawable.GradientDrawable
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.util.TypedValue
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,6 +20,7 @@ import android.widget.LinearLayout
 import android.widget.ScrollView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -30,14 +33,20 @@ import com.tsdc_vynils_app.app.viewModels.HomeViewModel
 import androidx.lifecycle.Observer
 import com.squareup.picasso.Transformation
 import com.tsdc_vynils_app.app.models.Album
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
     private lateinit var homeViewModel:HomeViewModel
+    private lateinit var Albums: List<Album>
 
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -52,12 +61,166 @@ class HomeFragment : Fragment() {
         root.setBackgroundColor(resources.getColor(R.color.black))
 
 
+
+
+        val scrollView = binding.contentAlbums
+
+        scrollView.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
+            if (scrollY == 0) {
+
+                getAllAlbums()
+            }
+        }
+
+
         return root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val parentLinearLayout: LinearLayout = binding.parentLinearLayout
+    fun getAllAlbums(){
+        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+
+            try {
+
+                homeViewModel.albums.observe(viewLifecycleOwner, Observer<List<Album>> {
+                    it.apply {
+                        Albums=it
+                        GlobalScope.launch(Dispatchers.IO) {
+                            withContext(Dispatchers.Main) {
+                                updateUI(Albums)
+                                binding.swipeRefreshLayout.isRefreshing = false
+                            }
+                        }
+                    }
+
+
+
+                })
+                homeViewModel.eventNetworkError.observe(viewLifecycleOwner, Observer<Boolean> { isNetworkError ->
+                    if (isNetworkError) onNetworkError()
+                })
+
+
+
+
+
+            } catch (e: Exception) {
+                Log.d("error",e.toString())
+
+                    onNetworkError()
+
+            }
+
+    }
+
+    fun updateUI(albums: List<Album>) {
+        val parentLinearLayout: LinearLayout = binding.parentContentLinearLayout
+        parentLinearLayout.removeAllViews()
+
+        var childLinearLayout = LinearLayout(requireContext())
+
+
+        var cont=1
+
+        albums.forEach{ al->
+            //cargando los albumes
+            if(cont==4 || cont==1){
+                cont=1
+                childLinearLayout = LinearLayout(requireContext())
+                val layoutParams = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT, // Ancho
+                    LinearLayout.LayoutParams.WRAP_CONTENT // Altura
+                )
+
+                val marginTopInDp = 10 // Valor en dp
+                val marginTopInPixels = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    marginTopInDp.toFloat(),
+                    resources.displayMetrics
+                ).toInt()
+                val marginLeftInDp = 20 // Valor en dp
+                val marginLeftPixels = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    marginLeftInDp.toFloat(),
+                    resources.displayMetrics
+                ).toInt()
+
+                layoutParams.setMargins(marginLeftPixels, marginTopInPixels, 0, 0)
+                childLinearLayout.layoutParams = layoutParams
+                childLinearLayout.orientation = LinearLayout.HORIZONTAL
+                parentLinearLayout.addView(childLinearLayout)
+            }
+            val imageWidth = 100 // Valor en dp
+            val imageSizeInPixels = TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                imageWidth.toFloat(),
+                resources.displayMetrics).toInt()
+
+            val imageUrl = al.cover
+
+
+            val cardLinearLayout = LinearLayout(requireContext())
+
+            val layoutParamsCard = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT)
+            val marginlefcardtindp = 5 // Valor en dp
+            val marginlefcardInPixels = TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                marginlefcardtindp.toFloat(),
+                resources.displayMetrics).toInt()
+
+            layoutParamsCard.setMargins(marginlefcardInPixels, 0, marginlefcardInPixels, 0)
+            cardLinearLayout.layoutParams = layoutParamsCard
+            cardLinearLayout.orientation = LinearLayout.VERTICAL
+
+
+            val albumImageView = ImageView(requireContext())
+            albumImageView.layoutParams = ViewGroup.LayoutParams(
+                imageSizeInPixels,
+                imageSizeInPixels
+            )
+            albumImageView.setOnClickListener {
+                val action = R.id.action_navigation_home_to_navigation_album_details
+                albumImageView.findNavController().navigate(action) }
+
+
+
+            Picasso.get()
+                .load(imageUrl)
+                .into(albumImageView)
+
+
+
+
+
+
+            cardLinearLayout.addView(albumImageView)
+
+            val textViewAlbum = TextView(requireContext())
+            textViewAlbum.text = al.name
+            textViewAlbum.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+            val fontSize = 8 // Valor en dp
+            val fontSizeInPixels = TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                fontSize.toFloat(),
+                resources.displayMetrics).toInt()
+            textViewAlbum.setTextSize(TypedValue.COMPLEX_UNIT_PX, fontSizeInPixels.toFloat())
+            cardLinearLayout.addView(textViewAlbum)
+
+            childLinearLayout.addView(cardLinearLayout)
+            cont++
+
+
+
+        }
+
+
+    }
+
+
+    fun fillDataInLayout(){
+        val parentLinearLayout: LinearLayout = binding.parentContentLinearLayout
+        parentLinearLayout.removeAllViews()
 
         var childLinearLayout = LinearLayout(requireContext())
 
@@ -97,9 +260,9 @@ class HomeFragment : Fragment() {
                     }
                     val imageWidth = 100 // Valor en dp
                     val imageSizeInPixels = TypedValue.applyDimension(
-                         TypedValue.COMPLEX_UNIT_DIP,
-                         imageWidth.toFloat(),
-                         resources.displayMetrics).toInt()
+                        TypedValue.COMPLEX_UNIT_DIP,
+                        imageWidth.toFloat(),
+                        resources.displayMetrics).toInt()
 
                     val imageUrl = al.cover
 
@@ -107,13 +270,13 @@ class HomeFragment : Fragment() {
                     val cardLinearLayout = LinearLayout(requireContext())
 
                     val layoutParamsCard = LinearLayout.LayoutParams(
-                         LinearLayout.LayoutParams.WRAP_CONTENT,
-                         LinearLayout.LayoutParams.WRAP_CONTENT)
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT)
                     val marginlefcardtindp = 5 // Valor en dp
                     val marginlefcardInPixels = TypedValue.applyDimension(
-                         TypedValue.COMPLEX_UNIT_DIP,
-                         marginlefcardtindp.toFloat(),
-                         resources.displayMetrics).toInt()
+                        TypedValue.COMPLEX_UNIT_DIP,
+                        marginlefcardtindp.toFloat(),
+                        resources.displayMetrics).toInt()
 
                     layoutParamsCard.setMargins(marginlefcardInPixels, 0, marginlefcardInPixels, 0)
                     cardLinearLayout.layoutParams = layoutParamsCard
@@ -122,12 +285,12 @@ class HomeFragment : Fragment() {
 
                     val albumImageView = ImageView(requireContext())
                     albumImageView.layoutParams = ViewGroup.LayoutParams(
-                         imageSizeInPixels,
-                         imageSizeInPixels
-                     )
+                        imageSizeInPixels,
+                        imageSizeInPixels
+                    )
                     albumImageView.setOnClickListener {
-                         val action = R.id.action_navigation_home_to_navigation_album_details
-                         albumImageView.findNavController().navigate(action) }
+                        val action = R.id.action_navigation_home_to_navigation_album_details
+                        albumImageView.findNavController().navigate(action) }
 
 
 
@@ -147,9 +310,9 @@ class HomeFragment : Fragment() {
                     textViewAlbum.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
                     val fontSize = 8 // Valor en dp
                     val fontSizeInPixels = TypedValue.applyDimension(
-                         TypedValue.COMPLEX_UNIT_DIP,
-                         fontSize.toFloat(),
-                         resources.displayMetrics).toInt()
+                        TypedValue.COMPLEX_UNIT_DIP,
+                        fontSize.toFloat(),
+                        resources.displayMetrics).toInt()
                     textViewAlbum.setTextSize(TypedValue.COMPLEX_UNIT_PX, fontSizeInPixels.toFloat())
                     cardLinearLayout.addView(textViewAlbum)
 
@@ -159,13 +322,18 @@ class HomeFragment : Fragment() {
 
 
                 }
-
             }
-
         })
         homeViewModel.eventNetworkError.observe(viewLifecycleOwner, Observer<Boolean> { isNetworkError ->
             if (isNetworkError) onNetworkError()
         })
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        getAllAlbums()
 
 
     }
@@ -173,8 +341,48 @@ class HomeFragment : Fragment() {
     private fun onNetworkError() {
         if(!homeViewModel.isNetworkErrorShown.value!!) {
             Toast.makeText(activity, "Error de conexión", Toast.LENGTH_LONG).show()
+            showConnectionError()
             homeViewModel.onNetworkErrorShown()
         }
+    }
+
+    private fun showConnectionError(){
+        val parentLinearLayout: LinearLayout = binding.parentContentLinearLayout
+        val WifiImage = ImageView(requireContext())
+        val imageWidth = 100
+        val imageSizeInPixels = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            imageWidth.toFloat(),
+            resources.displayMetrics).toInt()
+        val layoutParams = LinearLayout.LayoutParams(
+            imageSizeInPixels,
+            imageSizeInPixels
+        )
+        layoutParams.gravity = Gravity.CENTER
+        WifiImage.layoutParams = layoutParams
+        Picasso.get().load(R.drawable.ic_nowifi).into(WifiImage)
+
+        parentLinearLayout.addView(WifiImage)
+
+        val textMessage = TextView(requireContext())
+        textMessage.text = "No tienes conexión a internet"
+        textMessage.setTextColor(ContextCompat.getColor(requireContext(), R.color.colorTextSelected))
+        val fontSize = 20
+        val fontSizeInPixels = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            fontSize.toFloat(),
+            resources.displayMetrics).toInt()
+        textMessage.setTextSize(TypedValue.COMPLEX_UNIT_PX, fontSizeInPixels.toFloat())
+        val textLayoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        textLayoutParams.gravity = Gravity.CENTER
+
+        textMessage.layoutParams = textLayoutParams
+
+        parentLinearLayout.addView(textMessage)
+
     }
 
     override fun onDestroyView() {
