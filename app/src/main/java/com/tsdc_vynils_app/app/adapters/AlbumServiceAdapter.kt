0e044970b527c80472b6a0641b3ff78da1c1045b
zearkiatos <caprilespe@outlook.com>
2023-11-04 00:11:32 -0300

    Service adapterwork in progress

---
+++ app/src/main/java/com/tsdc_vynils_app/app/adapters/AlbumServiceAdapter.kt
@@ -0,0 +1,20 @@
+package com.tsdc_vynils_app.app.adapters
+
+import android.content.Context
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.tsdc_vynils_app.app.models.Album
+import com.tsdc_vynils_app.app.models.Performer
+import com.tsdc_vynils_app.app.models.Comment
+import org.json.JSONArray
+import org.json.JSONObject
+
+class AlbumServiceAdapter {
+
+}
