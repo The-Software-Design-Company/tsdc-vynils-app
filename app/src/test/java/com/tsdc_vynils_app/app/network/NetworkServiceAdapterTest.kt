24a441aa758778f355a84064833949db6fb9f0ba
fabio Castagnola Valencia <fcastagnola@namutek.com>
2023-11-05 17:14:16 -0500

    Final unit tests for network service

--- app/src/test/java/com/tsdc_vynils_app/app/network/NetworkServiceAdapterTest.kt
+++ app/src/test/java/com/tsdc_vynils_app/app/network/NetworkServiceAdapterTest.kt
@@ -30,7 +30,7 @@ class NetworkServiceAdapterTest {
     }
 
     @Test
-    fun `when The Api  Return Something Then Get from api`() = runBlocking {
+    fun `when The Api  Return Something Then Get Albums from api`() = runBlocking {
         val expectedComments = listOf<Comment>()
         val expectedTrack = listOf<Track>()
         val expectedPerformers = listOf<Performer>()
@@ -56,10 +56,8 @@ class NetworkServiceAdapterTest {
             onComplete(expectedAlbums)
         }
 
-        // Mockear el resultado de la función refreshData
         coEvery { albumRepository.refreshData(any(), any()) } coAnswers {
             val onComplete: (List<Album>) -> Unit = arg(0)
-
             onComplete.invoke(expectedAlbums)
         }
         // When
@@ -73,9 +71,88 @@ class NetworkServiceAdapterTest {
 
         // Then
         coVerify(exactly = 1) { albumRepository.refreshData(any(), any()) }
+        assertNotNull(result)
         assertEquals(expectedAlbums, result)
+        //assertEquals(200, statusCode)
     }
 
+    @Test
+    fun `when The Api  Return Something Then Get Album from api`() = runBlocking {
+        // Given
+        val expectedAlbumId = 1
+        val expectedAlbum = Album(
+            id = expectedAlbumId,
+            name = "Buscando América",
+            cover = "https://i.pinimg.com/564x/aa/5f/ed/aa5fed7fac61cc8f41d1e79db917a7cd.jpg",
+            releaseDate = "1984-08-01T00:00:00-05:00",
+            description = "Buscando América es el primer álbum de la banda de Rubén Blades y Seis del Solar lanzado en 1984...",
+            genre = "Salsa",
+            recordLabel = "Elektra",
+            comments = listOf<Comment>(),
+            tracks = listOf<Track>(),
+            performers = listOf<Performer>()
+        )
 
+        coEvery { networkServiceAdapter.getAlbum(any(), any(), any()) } coAnswers {
+            val albumId = arg<Int>(0)
+            val onComplete: (Album) -> Unit = arg(1)
+            val onError: (VolleyError) -> Unit = arg(2)
 
+            if (albumId == expectedAlbumId) {
+                onComplete.invoke(expectedAlbum)
+            } else {
+                onError.invoke(VolleyError("Simulated API error"))
+            }
+        }
+
+        // When
+        var result: Album? = null
+        networkServiceAdapter.getAlbum(
+            albumId = expectedAlbumId,
+            onComplete = { album ->
+                result = album
+            },
+            onError = { /* maneja el error, si es necesario */ }
+        )
+
+        delay(1000)
+
+        // Then
+        coVerify(exactly = 1) { networkServiceAdapter.getAlbum(expectedAlbumId, any(), any()) }
+        assertNotNull(result)
+        assertEquals(expectedAlbum, result)
+    }
+    @Test
+    fun `when The Api Doesnt Return an Album Then Handle Error`() = runBlocking {
+        // Given
+        val unexpectedAlbumId = 999
+
+        coEvery { networkServiceAdapter.getAlbum(any(), any(), any()) } coAnswers {
+            val albumId = arg<Int>(0)
+            val onComplete: (Album) -> Unit = arg(1)
+            val onError: (VolleyError) -> Unit = arg(2)
+
+            if (albumId == unexpectedAlbumId) {
+                onError.invoke(VolleyError("Simulated API error"))
+            } else {
+                // Esto simula que la API no retornó un álbum
+            }
+        }
+
+        // When
+        var error: VolleyError? = null
+        networkServiceAdapter.getAlbum(
+            albumId = unexpectedAlbumId,
+            onComplete = { /* maneja el álbum, si es necesario */ },
+            onError = { e ->
+                error = e
+            }
+        )
+
+        delay(1000)
+
+        // Then
+        coVerify(exactly = 1) { networkServiceAdapter.getAlbum(unexpectedAlbumId, any(), any()) }
+        assertNotNull(error)
+    }
 }
