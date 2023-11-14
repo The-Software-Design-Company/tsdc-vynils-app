c4a06316f1afac7b52b7d10c3328a3016d3fc49e
zearkiatos <caprilespe@outlook.com>
2023-11-14 00:05:54 -0300

    Create activity of artist details

---
+++ app/src/main/java/com/tsdc_vynils_app/app/ui/artistDetails/ArtistDetailsActivity.kt
@@ -0,0 +1,34 @@
+package com.tsdc_vynils_app.app.ui.artistDetails
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.Toolbar
+import com.tsdc_vynils_app.app.R
+
+class ArtistDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_artist_details)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.setDisplayShowTitleEnabled(false)
+        actionBar?.hide()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val titleTextView = toolbar.findViewById<TextView>(R.id.toolbar_title)
+        titleTextView.text = getResources().getString(R.string.title_activity_artist_details)
+        val iconImageView = findViewById<AppCompatImageView>(R.id.toolbar_icon)
+        iconImageView.setImageDrawable(getDrawable(R.drawable.arrow_left))
+        iconImageView.setOnClickListener {
+            this.onSupportNavigateUp()
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed() // Handle the Up button click event
+        return true
+    }
+}
