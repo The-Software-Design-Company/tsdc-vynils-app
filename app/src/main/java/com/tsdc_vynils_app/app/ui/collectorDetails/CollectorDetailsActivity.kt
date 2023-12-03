12ac2fe4f259594a9b4cdeb894373876689d4e0e
migueltovar <loganitsolutions@outlook.com>
2023-12-02 19:30:45 -0500

    terminada integración

--- app/src/main/java/com/tsdc_vynils_app/app/ui/collectorDetails/CollectorDetailsActivity.kt
+++ app/src/main/java/com/tsdc_vynils_app/app/ui/collectorDetails/CollectorDetailsActivity.kt
@@ -2,68 +2,61 @@ package com.tsdc_vynils_app.app.ui.collectorDetails
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.ViewModelProvider
+import com.squareup.picasso.Picasso
 import com.tsdc_vynils_app.app.R
+import com.tsdc_vynils_app.app.databinding.ActivityArtistDetailsBinding
 import com.tsdc_vynils_app.app.databinding.ActivityCollectorDetailsBinding
+import com.tsdc_vynils_app.app.models.Collector
+import com.tsdc_vynils_app.app.models.Musician
+import com.tsdc_vynils_app.app.viewModels.ArtistDetailViewModel
 import com.tsdc_vynils_app.app.viewModels.CollectorDetailsViewModel
 
 
 class CollectorDetailsActivity: AppCompatActivity() {
 
+    private lateinit var viewModel: CollectorDetailsViewModel
     private lateinit var binding: ActivityCollectorDetailsBinding
-    private var _bindingData: ActivityCollectorDetailsBinding? = null
-
-    private val bindingData get() = _bindingData!!
-    private lateinit var collectorDetailsViewModel: CollectorDetailsViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         binding = ActivityCollectorDetailsBinding.inflate(layoutInflater)
-
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_collector_details)
-        setContentView(binding.root)
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         val actionBar: ActionBar? = supportActionBar
         actionBar?.setDisplayShowTitleEnabled(false)
         actionBar?.hide()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val bundle: Bundle? = intent.extras
+        val titleTextView = toolbar.findViewById<TextView>(R.id.toolbar_title)
+        titleTextView.text = getResources().getString(R.string.collectors_title_description)
 
-        if (bundle != null) {
-            val collectorId = bundle.getInt("collectorId", 0)
-            val viewModel = ViewModelProvider(this, CollectorDetailsViewModel.Factory(this.application, collectorId)).get(
-                CollectorDetailsViewModel::class.java)
-            val collectorName = this.findViewById<TextView>(R.id.collector_detail_name)
-            val collectorPhone = this.findViewById<TextView>(R.id.collector_detail_phone)
-            val collectorEmail = this.findViewById<TextView>(R.id.collector_detail_email)
-
-            viewModel.collector.observe(this, Observer { data ->
-                data.apply {
-                    collectorName.text = data.name
-                    collectorEmail.text = data.email
-                    collectorPhone.text = data.telephone
-                }
-            })
-
-            val titleTextView = toolbar.findViewById<TextView>(R.id.collector_detail_title)
-            titleTextView.text = "Detalle del Coleccionista"
-
-        } else {
-            Toast.makeText(this, "Argument is missing", Toast.LENGTH_SHORT).show()
-            Log.e("TAG", "Argument is missing")
+        val iconImageView = findViewById<AppCompatImageView>(R.id.toolbar_icon)
+        iconImageView.setImageDrawable(getDrawable(R.drawable.arrow_left))
+        iconImageView.setOnClickListener {
+            this.onSupportNavigateUp()
         }
-    }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        _bindingData = null
+        val collectorName=this.findViewById<TextView>(R.id.collector_detail_name)
+        val collectorPhone=this.findViewById<TextView>(R.id.collector_detail_phone)
+        val collectorEmail=this.findViewById<TextView>(R.id.collector_detail_email)
+
+
+        val receivedIntent = intent
+        val receivedObject = receivedIntent.getSerializableExtra("collector") as Collector?
+
+        if (receivedObject != null) {
+            collectorName.text=receivedObject.name
+            collectorPhone.text=receivedObject.telephone
+            collectorEmail.text=receivedObject.email
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
